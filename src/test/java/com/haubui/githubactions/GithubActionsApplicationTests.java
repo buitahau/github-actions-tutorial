229f01727e8aff1e70ab1b16c7fd6356ee09075f
buitahau <buitahau@gmail.com>
2024-02-20 22:08:43 +0700

    feat: add build action

--- src/test/java/com/haubui/githubactions/GithubActionsApplicationTests.java
+++ src/test/java/com/haubui/githubactions/GithubActionsApplicationTests.java
@@ -1,13 +1,17 @@
 package com.haubui.githubactions;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GithubActionsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {}
 
+    @Test
+    void failCase() {
+        Assertions.assertTrue(true);
+    }
 }
